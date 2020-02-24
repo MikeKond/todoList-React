31069668ed66852a21d6e7a141e43c8c99a34c67
Mikhail Kondrashov <mike.kond.02@mail.ru>
2020-02-24 23:24:30 +0300

    remake App component to class

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import TodoList from "../todo-list";
 import AppHeader from "../app-header";
@@ -7,25 +7,37 @@ import ItemStatusFilter from "../item-status-filter";
 
 import "./app.css";
 
-const App = () => {
+class App extends Component {
 
-  const todoData = [
-    { label: "Drink Coffee", important: false, id: 1 },
-    { label: "Build React App", important: true, id: 2 },
-    { label: "Have lunch", important: false, id: 3 }
-  ];
+  constructor() {
+    super();
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={ 1 } done={ 3 }/>
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+    this.state = {
+      todoData: [
+        { label: "Drink Coffee", important: false, id: 1 },
+        { label: "Build React App", important: true, id: 2 },
+        { label: "Have lunch", important: false, id: 3 }
+      ]
+    };
+
+  }
+
+  render() {
+    
+    const { todoData } = this.state;
+
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={ 1 } done={ 3 }/>
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+  
+        <TodoList todos={ todoData } />
       </div>
-
-      <TodoList todos={ todoData } />
-    </div>
-  );
+    );
+  }
 };
 
 export default App;
