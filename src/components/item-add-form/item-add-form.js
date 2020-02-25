9ab71fa08443a78a3a260a09a769f3354b4e61bd
Mikhail Kondrashov <mike.kond.02@mail.ru>
2020-02-25 11:44:55 +0300

    add styles and functional to change text in item-add-from once new item is adding

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,16 +4,49 @@ import "./item-add-form.css";
 
 export default class ItemAddForm extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      labelText: ""
+    };
+  }
+
   render() {
 
-    const { onAdded: addItem } = this.props;
+    const { onAdded: addItem } = this.props,
+          { labelText }        = this.state;
+
+    const onFormSubmit = (evt) => {
+      evt.preventDefault();
+
+      this.setState({
+        labelText: ""
+      });
+    }
+
+    const onLabelChange = ({ target: { value } }) => {
+      this.setState({
+        labelText: value
+      });
+    }
 
     return (
-      <div className="item-add-form">
+      <form 
+        className="item-add-form d-flex"
+        onSubmit={ (evt) => { onFormSubmit(evt) } }>
+
+        <input 
+          type="text"
+          className="form-control"
+          placeholder="What needs to be done"
+          value={ labelText }
+          onChange={ (evt) => { onLabelChange(evt) } }/>
+
         <button 
           className="btn btn-outline-secondary"
-          onClick={ () => { addItem("new el text"); } }>Add Item</button>
-      </div>
+          onClick={ () => { addItem(labelText); } }>Add Item</button>
+      </form>
     );
   }
 }
